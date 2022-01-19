4dd0a486792845d28b8f49405fa8937c7f967f4c
bajok <By3olaole?>
2022-01-19 10:15:58 -0500

    Completed HW 1 for CSCI 220

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,34 +1,48 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Ka'ala Bajo
+hw1.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: These take user input and run arithmatic on them to solve simple problems!
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def calc_rec_area():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
 
 
 def calc_volume():
-    pass
+    v_length = eval(input("Enter the length: "))
+    v_width = eval(input("Enter the width: "))
+    v_height = eval(input("Enter the height: "))
+    volume = v_height * v_length * v_width
+    print("Volume =", volume)
 
 
 def shooting_percentage():
-    pass
+    total_shots = eval(input("Enter the player's total shots: "))
+    shots_made = eval(input("Enter how many shots the player made: "))
+    shooting_per = (shots_made/total_shots) * 100
+    print("Shooting percentage: ", shooting_per,"%")
 
 
 def coffee():
-    pass
-
+    coffee_order = eval(input("How many pounds of coffee would you like? "))
+    raw_cost = (coffee_order * 10.50) + (coffee_order * .86) + 1.50
+    rounded_total = round(raw_cost, 2)
+    print("Your total is: $", rounded_total)
 
 def kilometers_to_miles():
-    pass
+    km_traveled = eval(input("How many kilometers did you travel? "))
+    miles = km_traveled / 1.61
+    print("That's", miles, "miles!")
+
+kilometers_to_miles()
 
 
 if __name__ == '__main__':
