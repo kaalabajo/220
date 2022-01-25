ed44ba5d2db1bc036cc2ccd4307ca5450698b98c
bajok <By3olaole?>
2022-01-25 16:30:01 -0500

    HW 2 for CSCI220

--- labs/lab2/lab2.py
+++ labs/lab2/lab2.py
@@ -1,23 +1,26 @@
+"""
+Name: Kaala Bajo
+lab2.py
+
+Problem: This program calculates 3 different means.
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
 def means():
     vals = eval(input("How many values do you need to enter? "))
 
-    #basic mean
-    #for i in range(vals):
-    #    sum = sum + eval(input("Enter value: "))
-    #mean = sum/vals
-
     #user_input = []
     #user_input_sqd = []
-    #for i in range(vals):
-    #    x = eval(input("Enter a number: "))
-    #    vals_sqrd = x ** 2
-    #    user_input.append(vals_sqrd)
-    #    user_input_sqd.append(x)
-    number_current = 0
-    for j in range(3):
-        number_current = eval(input("Enter a number"))
 
-        #RMS
+    rms_acc = 0
+    harm_acc = 0
+    for i in range(vals):
+        x = eval(input("Enter a number: ")
+        xsquare = x ** 2
+        rms_acc = rms_acc + xsquare
+
         xs_squared_sum = 0
         for i in range(vals):
             xs_squared_sum = xs_squared_sum + (eval(input("Enter a number: "))**2)#raises each to power of 2 and adds to self
@@ -29,9 +32,13 @@ def means():
         for i in range(vals):
             h_sum = h_sum + ( 1/ eval(input("Enter a number: ")) )
         harmonic = vals/h_sum
-        print(harmonic)
+        print("harmonic:", harmonic)
 
     #geometric
+    g_prod = 1
     #pi (x1*x2*x3)
-
+    for i in range(vals):
+        g_prod = g_prod * eval(input("Input a number: "))
+    geometric = g_prod**(1/vals)
+    print("Geometric:", geometric)
 means()
