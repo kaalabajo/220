5905fdce5b1b396b422de8586c0b6ff5483e6fab
bajok <By3olaole?>
2022-01-25 17:42:00 -0500

    HW 2 for CSCI220

--- labs/lab2/lab2.py
+++ labs/lab2/lab2.py
@@ -11,34 +11,17 @@ I certify that this assignment is entirely my own work.
 def means():
     vals = eval(input("How many values do you need to enter? "))
 
-    #user_input = []
-    #user_input_sqd = []
-
     rms_acc = 0
     harm_acc = 0
+
     for i in range(vals):
-        x = eval(input("Enter a number: ")
-        xsquare = x ** 2
-        rms_acc = rms_acc + xsquare
+        number = eval(input("Enter a number: "))
+        x_square = number ** 2
+        rms_acc = rms_acc + x_square
 
-        xs_squared_sum = 0
-        for i in range(vals):
-            xs_squared_sum = xs_squared_sum + (eval(input("Enter a number: "))**2)#raises each to power of 2 and adds to self
-        print("RMS:", (xs_squared_sum/vals)**.5)
+    print("RMS:", (rms_acc/vals)**.5)
 
-        #Harmonic
-        h_sum = 0
-        # summing 1/xn then div by n outside the loop
-        for i in range(vals):
-            h_sum = h_sum + ( 1/ eval(input("Enter a number: ")) )
-        harmonic = vals/h_sum
-        print("harmonic:", harmonic)
 
-    #geometric
-    g_prod = 1
-    #pi (x1*x2*x3)
-    for i in range(vals):
-        g_prod = g_prod * eval(input("Input a number: "))
-    geometric = g_prod**(1/vals)
-    print("Geometric:", geometric)
+
+
 means()
