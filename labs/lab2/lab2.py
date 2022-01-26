38021e6da2526b8aa4316530df63003167ad9e75
bajok <By3olaole?>
2022-01-25 20:14:13 -0500

    Completed HW 2 for CSCI220

--- labs/lab2/lab2.py
+++ labs/lab2/lab2.py
@@ -11,17 +11,23 @@ I certify that this assignment is entirely my own work.
 def means():
     vals = eval(input("How many values do you need to enter? "))
 
+    #here are my little variables to add up stuff
     rms_acc = 0
     harm_acc = 0
+    geom_acc = 1
 
     for i in range(vals):
         number = eval(input("Enter a number: "))
         x_square = number ** 2
         rms_acc = rms_acc + x_square
+        harm_acc = harm_acc + 1/number
+        geom_acc = geom_acc * number
+
+    geometric = geom_acc ** (1 / vals)
 
     print("RMS:", (rms_acc/vals)**.5)
-
-
+    print("Harmonic:", vals/harm_acc)
+    print("Geometric:", (geometric))
 
 
 means()
