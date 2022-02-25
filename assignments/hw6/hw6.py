5cbbd9a3c75f753acb59ec506d91f289c39ccb4b
bajok <By3olaole?>
2022-02-25 10:17:15 -0500

    hw 6 done!

--- assignments/hw6/hw6.py
+++ assignments/hw6/hw6.py
@@ -1,44 +1,71 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Kaala Bajo
+hw6.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
+Problem: This program solves a series of problems
+         involving strings and formatting.
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
-
+import math
 def cash_converter():
-    pass
-
+    the_int = eval(input("enter an integer "))
+    print("That is ${0:0.2f}".format(float(the_int)))
 
 def encode():
-    pass
-
+    my_msg = input("enter a message: ")
+    my_key = eval(input('enter a key: '))
+    code = ""
+    for letter in my_msg:
+        num = ord(letter)
+        code = code + chr(num+my_key)
+    print(code)
+#The time has come, the Walrus said
 
 def sphere_area(radius):
-    pass
-
+    #4pirad**2
+    return 4 * math.pi * (radius**2)
 
 def sphere_volume(radius):
-    pass
-
+    #4/3 pi r**2
+    return (4/3) * math.pi * (radius**3)
 
 def sum_n(number):
-    pass
-
+    num_acc = 0
+    for i in range(number+1):
+        num_acc = num_acc + i
+    return num_acc
 
 def sum_n_cubes(number):
-    pass
+    num_acc = 0
+    for i in range(number+1):
+        num_acc = num_acc + i**3
+    return num_acc
 
 
 def encode_better():
-    pass
+    code = input("enter a message: ")
+    key = input("enter a key: ")
 
+    track_code = []
+    track_key = []
+    for letter in code:
+        track_code.append(ord(letter) - 65)
+    for letter in key:
+        track_key.append(ord(letter) - 65)
+    #print(track_code,track_key)
+    # make a new list to keep track of the number versions
+    nums = []
+    for i in range(len(code)):
+        looped = int(track_code[i]) + int(track_key[i % len(key)])
+        nums.append(looped%58)
 
+    # ciphered is a blank string that will add all our letters
+    ciphered = ''
+    for i in nums:
+        ciphered = ciphered + chr(i+65)
+    print(ciphered)
 if __name__ == '__main__':
     # cash_converter()
     # encode()
